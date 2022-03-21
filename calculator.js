86628ed671b9ad4be56d16a5237f90638260738b
Mariol90 <mariolorenzo90@yahoo.com>
2022-03-21 18:49:28 -0400

    get input from Dom

--- calculator.js
+++ calculator.js
@@ -21,20 +21,30 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const values= getCurrentUIValues();
+  const useramout=0;
+  const useryear=0;
+  const userrate=0;
+  useramout.value= values.amount;
+  useryear.value=values.years;
+  userrate.value=values.rate;
+  calculateMonthlyPayment(values);
+update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
 }
-
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+ 
 }
